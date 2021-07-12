a790a1fdda336baabd221cf0e96c067bf1265b8b
webdevsmart <webdevsmart@hotmail.com>
2021-07-12 15:47:26 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+// const axios = require("axios");
+const { default: axios } = require("axios");
+const fs = require("fs");
+const { Parser } = require("json2csv");
+const json2csvParser = new Parser();
+
+let products = [];
+let flag = true;
+let i = 201;
+
+const shop = "shoclef-gold.myshopify.com";
+const accessToken = "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";
+let cursor = "";
+let query = "";
+async function getCollections() {
+  while (flag) {
+    if (cursor != "") {
+      let filter = `first:1,after:"${cursor}"`;
+      query = JSON.stringify({
+        query: `{
+        collections(${filter})
+        {
+          edges{
+            cursor
+            node {
+              title
+              description
+              image {
+                id
+                originalSrc
+              }
+            }
+          }
+        }
+      }`,
+      });
+    } else {
+      query = JSON.stringify({
+        query: `{
+          collections(first:1)
+          {
+            edges{
+              cursor
+              node {
+                title
+                description
+                image {
+                  id
+                  originalSrc
+                }
+              }
+            }
+          }
+        }`,
+      });
+    }
+    await axios
+      .post(`https://${shop}/admin/api/2020-10/graphql.json`, query, {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": accessToken,
+        },
+      })
+      .then(async (responseJson) => {
+        console.log(responseJson.data.data.collections.edges[0].node.title);
+        if (responseJson.data.data.collections.edges.length > 0) {
+          products.push(responseJson.data.data.collections.edges[0]);
+          cursor = responseJson.data.data.collections.edges[0].cursor;
+          if (cursor == "") {
+            flag = false;
+          }
+        }
+      });
+  }
+  PushCsv(111);
+}
+
+async function PushCsv(fileIndex) {
+  const jsonString = JSON.stringify(products);
+
+  await fs.writeFile(`./collections${fileIndex}.json`, jsonString, (err) => {
+    if (err) {
+      console.log("Error writing file", err);
+    } else {
+      console.log("Successfully wrote file", fileIndex);
+    }
+  });
+}
+
+getCollections();
